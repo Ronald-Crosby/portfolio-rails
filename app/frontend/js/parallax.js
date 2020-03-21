933c6db83a4b32e2dd016d691b1f8931e7631959
Olly Robinson <ollythedeveloper@gmail.com>
2020-03-21 23:55:36 +0000

    transfer html and js files, add img helpers

---
+++ app/frontend/js/parallax.js
@@ -0,0 +1,54 @@
+const $$elements = document.querySelectorAll('.js-parallax')
+
+let body = document.querySelector('.body')
+console.log(body)
+let bodyTop = document.querySelector('body').getBoundingClientRect().top
+
+let parallaxData = []
+
+function getData() {
+  $$elements.forEach($element => {
+    // the position of the top of the element from the top of the viewport
+    const elTopFromVP = $element.getBoundingClientRect().top
+    // top of the element from the top of the body tag
+    const elementTopFromBody = elTopFromVP - bodyTop
+    // bottom of the element from the top of the body tag
+    const elementBottomFromBody = elementTopFromBody + $element.getBoundingClientRect().height
+
+    parallaxData.push({
+      elem: $element,
+      elTop: parseInt(elementTopFromBody),
+      elBottom: parseInt(elementBottomFromBody),
+      speed: parseFloat($element.getAttribute('data-parallax')),
+      isRotate: $element.classList.contains('parallax-rotate'),
+      isScroll: $element.classList.contains('parallax-scroll'),
+    })
+  })
+}
+
+const updateParallax = () => {
+  const windowBottom = window.pageYOffset + window.innerHeight
+
+  parallaxData.forEach(data => {
+    if (windowBottom >= data.elTop && data.elBottom >= window.pageYOffset) {
+      // console.log('the element should be in view')
+      if (data.isRotate) {
+        data.elem.style.transform = `rotate(${(data.elBottom - windowBottom) * data.speed}deg)`
+      } else if (data.isScroll) {
+        data.elem.style.transform = `translate3d(0, ${(data.elTop - windowBottom) * data.speed}px, 0)`
+      }
+    }
+  })
+}
+
+window.addEventListener('scroll', updateParallax)
+getData()
+
+function checkScreen() {
+  if (window.innerWidth > 768) {
+    updateParallax()
+  }
+}
+
+checkScreen()
+window.addEventListener('resize', checkScreen())
---
+++ app/frontend/packs/scripts.js
@@ -0,0 +1,2 @@
+import '../js/background'
+import '../js/parallax'
