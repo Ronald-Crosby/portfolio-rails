1eaa3ddbe87763acbc9873fa105dd9369336e7c1
Olly Robinson <ollythedeveloper@gmail.com>
2020-03-22 12:36:45 +0000

    initialise global header

--- app/frontend/js/background.js
+++ app/frontend/js/background.js
@@ -1,23 +1,25 @@
-const backgroundTag = document.getElementById('background')
-const colorSections = document.querySelectorAll('[data-color]')
+document.addEventListener('DOMContentLoaded', function() {
+  const backgroundTag = document.getElementById('background')
+  const colorSections = document.querySelectorAll('[data-color]')
 
-let colorSectionData = []
+  let colorSectionData = []
 
-colorSections.forEach(section => {
-  colorSectionData.push({
-    offset: parseInt(section.getBoundingClientRect().top + window.pageYOffset),
-    classes: section.getAttribute('data-color'),
+  colorSections.forEach(section => {
+    colorSectionData.push({
+      offset: parseInt(section.getBoundingClientRect().top + window.pageYOffset),
+      classes: section.getAttribute('data-color'),
+    })
   })
+
+  const updateBackground = () => {
+    const windowScroll = window.pageYOffset
+    colorSectionData.forEach(({ offset, classes }) => {
+      if (windowScroll + 500 >= offset) {
+        backgroundTag.className = classes
+      }
+    })
+  }
+
+  document.addEventListener('scroll', updateBackground)
+  updateBackground()
 })
-
-const updateBackground = () => {
-  const windowScroll = window.pageYOffset
-  colorSectionData.forEach(({ offset, classes }) => {
-    if (windowScroll + 500 >= offset) {
-      backgroundTag.className = classes
-    }
-  })
-}
-
-document.addEventListener('scroll', updateBackground)
-updateBackground()
